950829b59dcbc07bad4835878ca8c994f34efb87
zhangyb <1259478500@qq.com>
2021-01-07 09:36:01 +0800

    'Home-TabControl'

---
+++ src/components/content/tabcontrol/TabControl.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="tabcontrol">
+    <div v-for="(item,index) in contents" class="tabcontrol-item"
+         :class="{active:currentindex==index}"
+          @click="itemclick(index)">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabControl",
+    props:{
+      contents:{
+        type:Array,
+        default(){
+          return []
+        }
+      }
+    },
+    data(){
+      return{
+        currentindex:0
+      }
+    },
+    methods:{
+      itemclick(index){
+        this.currentindex =index
+      },
+    }
+  }
+</script>
+
+<style scoped>
+.tabcontrol {
+  display: flex;
+  text-align: center;
+  height: 40px;
+  line-height: 40px;
+  font-size: 12px;
+  font-weight: 700;
+}
+
+.tabcontrol-item {
+  flex: 1;
+
+}
+
+.tabcontrol-item span {
+  padding-bottom: 5px;
+}
+
+  .active {
+    color: var(--color-high-text);
+  }
+
+.active span {
+  border-bottom:2px solid var(--color-high-text);
+}
+</style>
